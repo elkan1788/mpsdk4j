1c3cac1ff24d96546a0121f073aaff6c1d0a6fda
凡梦星尘 <elkan1788@gmail.com>
2014-12-15 14:51:16 +0800

    Fixed 因网络传输导致的字符编码集问题,同时修改缓冲区大小

--- src/main/java/org/elkan1788/osc/weixin/mp/util/StreamTool.java
+++ src/main/java/org/elkan1788/osc/weixin/mp/util/StreamTool.java
@@ -1,8 +1,12 @@
 package org.elkan1788.osc.weixin.mp.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 /**
  * 输入流与字符串处理工具
@@ -13,6 +17,8 @@ import java.io.InputStream;
  */
 public class StreamTool {
 
+    private static final Logger log = LoggerFactory.getLogger(StreamTool.class);
+
     /**
      * 将字符串转换成输入流
      *
@@ -20,7 +26,14 @@ public class StreamTool {
      * @return 输入流
      */
     public static InputStream toStream(String str) {
-        InputStream stream = new ByteArrayInputStream(str.getBytes());
+        InputStream stream = null;
+        try {
+            // UTF-8 解决网络传输中的字符集问题
+            stream = new ByteArrayInputStream(str.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            log.error("转换输出Stream异常,不支持的字符集!!!");
+            log.error(e.getLocalizedMessage(), e);
+        }
         return stream;
     }
 
@@ -31,11 +44,17 @@ public class StreamTool {
      * @return 字符串
      * @throws IOException
      */
-    public static String toString(InputStream is) throws IOException {
+    public static String toString(InputStream is) {
         StringBuffer str = new StringBuffer();
-        byte[] b = new byte[4096];
-        for (int n; (n = is.read(b)) != -1; ) {
-            str.append(new String(b, 0, n));
+        byte[] b = new byte[1024];
+
+        try {
+            for (int n; (n = is.read(b)) != -1; ) {
+                str.append(new String(b, 0, n));
+            }
+        } catch (IOException e) {
+            log.error("读取输入流出现异常!!!");
+            log.error(e.getLocalizedMessage(), e);
         }
         return str.toString();
     }
