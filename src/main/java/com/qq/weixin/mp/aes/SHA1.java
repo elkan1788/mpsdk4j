25aaa5327f6db382899364e84e9deb6c380c33f7
凡梦星尘 <elkan1788@gmail.com>
2015-02-27 11:30:04 +0800

    更新SHA1签名算法

--- src/main/java/com/qq/weixin/mp/aes/SHA1.java
+++ src/main/java/com/qq/weixin/mp/aes/SHA1.java
@@ -2,6 +2,7 @@ package com.qq.weixin.mp.aes;
 
 import java.security.MessageDigest;
 import java.util.Arrays;
+import java.util.Formatter;
 
 /**
  * SHA1算法计算公众平台的消息签名接口
@@ -11,40 +12,40 @@ import java.util.Arrays;
  */
 public class SHA1 {
 
-	/**
+    /**
      * 用SHA1算法生成安全签名
+     *
      * @param params [token, timestamp, nonce, encrypt]
      * @return 安全签名
      * @throws com.qq.weixin.mp.aes.AesException
      */
-        public static String calculate(String... params) throws AesException {
-		try {
+    public static String calculate(String... params) throws AesException {
+        try {
             String[] array = params;
-			StringBuffer sb = new StringBuffer();
-			// 字符串排序
-			Arrays.sort(array);
-			int len = params.length;
-			for (int i = 0; i < len; i++) {
-				sb.append(array[i]);
-			}
-			String str = sb.toString();
-			// SHA1签名生成
-			MessageDigest md = MessageDigest.getInstance("SHA-1");
-			md.update(str.getBytes());
-			byte[] digest = md.digest();
+            StringBuffer sb = new StringBuffer();
+            // 字符串排序
+            Arrays.sort(array);
+            int len = params.length;
+            for (int i = 0; i < len; i++) {
+                sb.append(array[i]);
+            }
 
-			StringBuffer hexstr = new StringBuffer();
-			String shaHex = "";
-			for (int i = 0; i < digest.length; i++) {
-				shaHex = Integer.toHexString(digest[i] & 0xFF);
-				if (shaHex.length() < 2) {
-					hexstr.append(0);
-				}
-				hexstr.append(shaHex);
-			}
-			return hexstr.toString();
-		} catch (Exception e) {
-			throw new AesException(AesException.ComputeSignatureError);
-		}
-	}
+            // SHA1签名生成
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            md.reset();
+            md.update(new String(sb).getBytes("UTF-8"));
+
+            // HEX输出
+            byte[] hash = md.digest();
+            Formatter formatter = new Formatter();
+            for (byte b : hash) {
+                formatter.format("%02x", b);
+            }
+            String hex = formatter.toString();
+            formatter.close();
+            return hex;
+        } catch (Exception e) {
+            throw new AesException(AesException.ComputeSignatureError);
+        }
+    }
 }
