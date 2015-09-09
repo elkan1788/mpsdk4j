a49fe92a89faa5adf96fefa59bca2aee8150035e
凡梦星尘 <elkan1788@gmail.com>
2015-09-09 17:07:52 +0800

    删除无用的API返回判断功能

--- src/main/java/io/github/elkan1788/mpsdk4j/vo/ApiResult.java
+++ src/main/java/io/github/elkan1788/mpsdk4j/vo/ApiResult.java
@@ -1,12 +1,12 @@
 package io.github.elkan1788.mpsdk4j.vo;
 
+import io.github.elkan1788.mpsdk4j.util.ConfigReader;
+
 import java.util.Map;
 
 import org.nutz.json.Json;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
-
-import io.github.elkan1788.mpsdk4j.util.ConfigReader;
 
 /**
  * 封装微信api返回结果, 输出实体类
@@ -17,66 +17,60 @@ import io.github.elkan1788.mpsdk4j.util.ConfigReader;
 @SuppressWarnings("unchecked")
 public class ApiResult {
 
-    private static final Log log = Logs.get();
+	private static final Log log = Logs.get();
 
-    private static ConfigReader _cr;
+	private static ConfigReader _cr;
 
-    static {
-        _cr = new ConfigReader("/ErrorCode.properties");
-    }
+	static {
+		_cr = new ConfigReader("/ErrorCode.properties");
+	}
 
-    private Map<String, Object> content;
-    private String json;
-    private Integer errCode;
-    private String errMsg;
-    private String errCNMsg;
+	private Map<String, Object> content;
+	private String json;
+	private Integer errCode;
+	private String errMsg;
+	private String errCNMsg;
 
-    public ApiResult(String json) {
-        this.json = json;
-        this.content = Json.fromJson(Map.class, json);
-        this.errCode = (Integer) this.content.get("errcode");
-        this.errMsg = (String) this.content.get("errmsg");
-        this.errCNMsg = this.errCode == null ? "" : _cr.get(String.valueOf(this.errCode));
+	public ApiResult(String json) {
+		this.json = json;
+		this.content = Json.fromJson(Map.class, json);
+		this.errCode = (Integer) this.content.get("errcode");
+		this.errMsg = (String) this.content.get("errmsg");
+		this.errCNMsg = this.errCode == null ? "请求成功." : _cr.get(String
+				.valueOf(this.errCode));
 
-        if (log.isInfoEnabled()) {
-            log.infof("Wechat api result: %s", json);
-            log.infof("%s", this.getErrCNMsg());
-        }
-    }
+		if (log.isInfoEnabled()) {
+			log.infof("Wechat api result: %s", json);
+			log.infof("%s", this.getErrCNMsg());
+		}
+	}
 
-    public static ApiResult create(String json) {
-        return new ApiResult(json);
-    }
+	public static ApiResult create(String json) {
+		return new ApiResult(json);
+	}
 
-    public Map<String, Object> getContent() {
-        return content;
-    }
+	public Map<String, Object> getContent() {
+		return content;
+	}
 
-    public String getJson() {
-        return json;
-    }
+	public String getJson() {
+		return json;
+	}
 
-    public Integer getErrCode() {
-        return this.errCode;
-    }
+	public Integer getErrCode() {
+		return this.errCode;
+	}
 
-    public String getErrMsg() {
-        return this.errMsg == null ? "Unknow Error!" : this.errMsg;
-    }
+	public String getErrMsg() {
+		return this.errMsg == null ? "Unknow Error!" : this.errMsg;
+	}
 
-    public String getErrCNMsg() {
-        return this.errCNMsg == null ? "未知错误!" : this.errCNMsg;
-    }
+	public String getErrCNMsg() {
+		return this.errCNMsg == null ? "未知错误!" : this.errCNMsg;
+	}
 
-    public boolean isSuccess() {
-        return (this.errCode == null || this.errCode.intValue() == 0);
-    }
+	public boolean isSuccess() {
+		return (this.errCode == null || this.errCode.intValue() == 0);
+	}
 
-    public boolean isAccessTokenInvalid() {
-        return this.errCode != null
-               && (this.errCode.intValue() == 40001
-                   || this.errCode.intValue() == 42001
-                   || this.errCode.intValue() == 42002
-                   || this.errCode.intValue() == 40014);
-    }
 }
--- src/test/java/io/github/elkan1788/mpsdk4j/vo/ApiResultTest.java
+++ src/test/java/io/github/elkan1788/mpsdk4j/vo/ApiResultTest.java
@@ -9,71 +9,70 @@ import org.junit.Test;
  */
 public class ApiResultTest {
 
-    private String sucJson = "{'errcode':0,'errmsg':'Success.'}";
-    private String unKnowErrJson = "{'errcode':9999,'errmsg':'Unkonw error.'}";
-    private String errJson = "{'errcode':-1,'errmsg':'System busy.'}";
-    private String atErrJson1 = "{'errcode':40001,'errmsg':'invalid appsecret'}";
-    private String atErrJson2 = "{'errcode':42001,'errmsg':'access_token over time'}";
-    private String atErrJson3 = "{'errcode':42002,'errmsg':'refresh token over time'}";
-    private String atErrJson4 = "{'errcode':40014,'errmsg':'invalid access_token'}";
+	private String sucJson = "{'errcode':0,'errmsg':'Success.'}";
+	private String unKnowErrJson = "{'errcode':9999,'errmsg':'Unkonw error.'}";
+	private String errJson = "{'errcode':-1,'errmsg':'System busy.'}";
+	private String atErrJson1 = "{'errcode':40001,'errmsg':'invalid appsecret'}";
+	private String atErrJson2 = "{'errcode':42001,'errmsg':'access_token over time'}";
+	private String atErrJson3 = "{'errcode':42002,'errmsg':'refresh token over time'}";
+	private String atErrJson4 = "{'errcode':40014,'errmsg':'invalid access_token'}";
 
-    @Test
-    public void testGetJson() {
-        ApiResult ar = ApiResult.create(sucJson);
-        Assert.assertEquals(sucJson, ar.getJson());
-    }
+	@Test
+	public void testGetJson() {
+		ApiResult ar = ApiResult.create(sucJson);
+		Assert.assertEquals(sucJson, ar.getJson());
+	}
 
-    @Test
-    public void testGetErrCode() {
-        ApiResult ar = ApiResult.create(errJson);
-        Assert.assertEquals(-1, ar.getErrCode().intValue());
-    }
+	@Test
+	public void testGetErrCode() {
+		ApiResult ar = ApiResult.create(errJson);
+		Assert.assertEquals(-1, ar.getErrCode().intValue());
+	}
 
-    @Test
-    public void testGetErrMsg() {
-        ApiResult ar = ApiResult.create(errJson);
-        Assert.assertEquals("System busy.", ar.getErrMsg());
-    }
+	@Test
+	public void testGetErrMsg() {
+		ApiResult ar = ApiResult.create(errJson);
+		Assert.assertEquals("System busy.", ar.getErrMsg());
+	}
 
-    @Test
-    public void testGetErrCNMsg() {
-        ApiResult ar = ApiResult.create(sucJson);
-        Assert.assertEquals("请求成功.", ar.getErrCNMsg());
-    }
+	@Test
+	public void testGetErrCNMsg() {
+		ApiResult ar = ApiResult.create(sucJson);
+		Assert.assertEquals("请求成功.", ar.getErrCNMsg());
+	}
 
-    @Test
-    public void testIsSuccess() {
-        ApiResult ar = ApiResult.create(errJson);
-        Assert.assertTrue(!ar.isSuccess());
-    }
+	@Test
+	public void testIsSuccess() {
+		ApiResult ar = ApiResult.create(errJson);
+		Assert.assertTrue(!ar.isSuccess());
+	}
 
-    @Test
-    public void testIsAccessTokenInvalid() {
-        ApiResult ar = ApiResult.create(atErrJson1);
-        Assert.assertTrue(ar.isAccessTokenInvalid());
-        ar = ApiResult.create(atErrJson2);
-        Assert.assertTrue(ar.isAccessTokenInvalid());
-        ar = ApiResult.create(atErrJson3);
-        Assert.assertTrue(ar.isAccessTokenInvalid());
-        ar = ApiResult.create(atErrJson4);
-        Assert.assertTrue(ar.isAccessTokenInvalid());
-    }
+	@Test
+	public void testIsAccessTokenInvalid() {
+		ApiResult ar = ApiResult.create(atErrJson1);
+		Assert.assertFalse(ar.isSuccess());
+		ar = ApiResult.create(atErrJson2);
+		Assert.assertFalse(ar.isSuccess());
+		ar = ApiResult.create(atErrJson3);
+		Assert.assertFalse(ar.isSuccess());
+		ar = ApiResult.create(atErrJson4);
+		Assert.assertFalse(ar.isSuccess());
+	}
 
-    @Test
-    public void testUnknowError() {
-        ApiResult ar = ApiResult.create(unKnowErrJson);
-        Assert.assertEquals(unKnowErrJson, ar.getJson());
-        Assert.assertEquals("未知错误!", ar.getErrCNMsg());
-    }
+	@Test
+	public void testUnknowError() {
+		ApiResult ar = ApiResult.create(unKnowErrJson);
+		Assert.assertEquals(unKnowErrJson, ar.getJson());
+		Assert.assertEquals("未知错误!", ar.getErrCNMsg());
+	}
 
-    @Test
-    public void testSucWithoutErrCode() {
-        String json = "{'access_token':'ACCESS_TOKEN','expires_in':7200}";
-        ApiResult ar = ApiResult.create(json);
-        Assert.assertEquals(json, ar.getJson());
-        Assert.assertEquals("Unknow Error!", ar.getErrMsg());
-        Assert.assertTrue(ar.isSuccess());
-        Assert.assertFalse(ar.isAccessTokenInvalid());
-    }
+	@Test
+	public void testSucWithoutErrCode() {
+		String json = "{'access_token':'ACCESS_TOKEN','expires_in':7200}";
+		ApiResult ar = ApiResult.create(json);
+		Assert.assertEquals(json, ar.getJson());
+		Assert.assertEquals("Unknow Error!", ar.getErrMsg());
+		Assert.assertTrue(ar.isSuccess());
+	}
 
 }
