844971f229b3ca43482a38958147f76096f98cff
凡梦星尘 <elkan1788@gmail.com>
2015-09-17 23:52:55 +0800

    更新测试微信公众号信息为配置文件模式

--- src/test/java/io/github/elkan1788/mpsdk4j/api/APITestSupport.java
+++ src/test/java/io/github/elkan1788/mpsdk4j/api/APITestSupport.java
@@ -2,18 +2,21 @@ package io.github.elkan1788.mpsdk4j.api;
 
 import org.junit.Before;
 
+import io.github.elkan1788.mpsdk4j.util.ConfigReader;
 import io.github.elkan1788.mpsdk4j.vo.MPAccount;
 
 public class APITestSupport {
 
     protected MPAccount mpAct;
 
+    static ConfigReader _cr = new ConfigReader("/mpconf.properties");
+
     @Before
     public void init() {
         mpAct = new MPAccount();
-        mpAct.setMpId("");
-        mpAct.setAppId("");
-        mpAct.setAppSecret("");
+        mpAct.setMpId(_cr.get("mpId"));
+        mpAct.setAppId(_cr.get("appId"));
+        mpAct.setAppSecret(_cr.get("appSecret"));
     }
 
 }
